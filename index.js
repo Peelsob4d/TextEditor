7c76734ecf1d0d2bd7e53b24b1ef26bfa5cf11e2
Peelsob4d <peelsob4d@gmail.com>
2022-05-26 23:51:27 +0900

    태그 엘리먼츠 사이 텍스트 노드 일괄처리 추가

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 const selectedField = document.querySelector('.Selected');
 const textField = document.querySelector('.TextField');
 const boldButton = document.querySelector('.Bold');
+
 let selection;
 let range;
+
+function undoBold() {
+  const text = document.createTextNode(selection.toString());
+  range.deleteContents();
+  textField.childNodes.forEach(
+    (node) => node.innerText === '' && textField.removeChild(node),
+  );
+  range.insertNode(text);
+}
+
+function insertBold(tagString) {
+  let tag = document.createElement(tagString);
+  tag.textContent = selection.toString();
+  range.deleteContents();
+  range.insertNode(tag);
+  textField.childNodes.forEach(
+    (node) => node.innerText === '' && textField.removeChild(node),
+  );
+  selection.collapse(tag, 0);
+  selection.extend(tag, 1);
+}
+
 document.addEventListener('selectionchange', (e) => {
   selection = document.getSelection();
   range = selection.getRangeAt(0);
   selectedField.innerHTML = selection.toString();
+  console.log(selection);
 });
+
 boldButton.addEventListener('click', (e) => {
   const anchor = selection.anchorNode.tagName;
   const focus = selection.focusNode.tagName;
   const parent_anchor = selection.anchorNode.parentElement.tagName;
   const parent_focus = selection.focusNode.parentElement.tagName;
-
-  function insertBold() {
-    let bold = document.createElement('b');
-    bold.textContent = selection.toString();
-    range.deleteContents();
-    range.insertNode(bold);
-    textField.childNodes.forEach(
-      (node) => node.innerText === '' && textField.removeChild(node),
-    );
-    selection.collapse(bold, 0);
-    selection.extend(bold, 1);
-  }
-
-  function undoBold() {
-    const text = document.createTextNode(selection.toString());
-    console.log(text);
-    range.deleteContents();
-    textField.childNodes.forEach(
-      (node) => node.innerText === '' && textField.removeChild(node),
-    );
-    range.insertNode(text);
-  }
+  const base = selection.baseNode.tagName;
 
   (anchor === 'B' && focus === 'B') ||
   (parent_anchor === 'B' && parent_focus === 'B')
     ? undoBold()
-    : insertBold();
+    : insertBold('b');
+  ((anchor === 'B' && focus === 'B' && base !== 'B') ||
+    (parent_anchor === 'B' && parent_focus === 'B' && base !== 'B')) &&
+    insertBold('b');
 });
